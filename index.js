bf02713cb01564acdfe8b6df56b06dd5e4a91938
Moscato Luca Gabriele <moscato.lucagabriele@osdbsesto>
2020-01-24 10:05:56 +0100

    add button and btn.eventListener

---
+++ index.js
@@ -0,0 +1,11 @@
+var btn = document.createElement("button");
+btn.innerHTML="Clicca";
+
+var body = document.getElementsByTagName("body")[0];
+body.append(btn);
+
+btn.addEventListener("click", function() {
+
+    document.getElementById("h1").innerHTML = "ciao come stai";
+});
+
